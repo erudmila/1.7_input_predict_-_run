4d6feb8277e1cf925775df75c6ce2a100e26627b
erudmila <144694341+erudmila@users.noreply.github.com>
2024-03-21 12:15:17 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+#Task 1 - Add comments to the code below to predict what will happen when it is run.
+
+#Task 2 - add comments to the code to explain what the variable(s) are called and where
+# they are used. Make sure to show where the variable.
+
+#this will show on the console the phrase " Hi! What's your name?"
+print("Hi! What's your name?")
+
+name = input("Elma")
+#^variable
+
+print("Hi " + name + "! How are you today?")
